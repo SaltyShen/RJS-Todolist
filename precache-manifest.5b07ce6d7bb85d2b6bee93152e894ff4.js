016b00fcbf5b3a1f6f09ff5f0af60db882168803
Shen Wang <peterhuiwang1995@gmail.com>
2020-05-15 17:40:12 -0500

    Updates

---
+++ precache-manifest.5b07ce6d7bb85d2b6bee93152e894ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb6c929896d28f99b0cabe7010f7a18",
+    "url": "/RJS-Todolist/index.html"
+  },
+  {
+    "revision": "2d58fc270789a4e0ffbb",
+    "url": "/RJS-Todolist/static/css/main.77d0625a.chunk.css"
+  },
+  {
+    "revision": "7c4743b3b8c9582bcfdb",
+    "url": "/RJS-Todolist/static/js/2.7325c625.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RJS-Todolist/static/js/2.7325c625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d58fc270789a4e0ffbb",
+    "url": "/RJS-Todolist/static/js/main.13cf1e41.chunk.js"
+  },
+  {
+    "revision": "203059bb7540dacd252d",
+    "url": "/RJS-Todolist/static/js/runtime-main.c9864a3d.js"
+  }
+]);
